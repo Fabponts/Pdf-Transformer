bbaf1010d43d83f998ae5dc4474ace25e28daf00
Fabricio Pontes <fabriciopontes02.fa@gmail.com>
2026-01-19 09:55:12 -0300

    feat(fxml and main): Add a new custom screen with fxml file

---
+++ src/main/java/org/example/pdftransformer/Main.java
@@ -0,0 +1,30 @@
+package org.example.pdftransformer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        try{
+            Parent parent = FXMLLoader.load(getClass().getResource("/org/example/pdftransformer/gui/view.fxml"));
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.setTitle("Pdf transformer");
+            stage.show();
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
